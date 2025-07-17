23b1068c95e33176ecf116a2de68533c12a70506
github-gmm <github-gmm@users.noreply.github.com>
2025-07-17 13:06:30 +0000

    deploy: ee756f6cd8f78b58f082456ae7373fd0dd043c7d

---
+++ meta__docs.f5977e4f.async.js
@@ -0,0 +1,12 @@
+"use strict";(self.webpackChunkhst_react_ui=self.webpackChunkhst_react_ui||[]).push([[904],{1584:function(a,t,n){n.r(t),n.d(t,{demos:function(){return u}});var d=n(67294),u={}},82213:function(a,t,n){n.r(t),n.d(t,{demos:function(){return u}});var d=n(67294),u={}},44401:function(a,t,n){n.r(t),n.d(t,{demos:function(){return u}});var d=n(67294),u={}},11171:function(a,t,n){n.r(t),n.d(t,{demos:function(){return u}});var d=n(67294),u={}},87167:function(a,t,n){n.r(t),n.d(t,{texts:function(){return d}});const d=[{value:"2025/07/17 ",paraId:0,tocIndex:0},{value:"hst-react-ui",paraId:0,tocIndex:0},{value:"  v1.0.0 \u6B63\u5F0F\u53D1\u5E03\u3002",paraId:0,tocIndex:0}]},77888:function(a,t,n){n.r(t),n.d(t,{texts:function(){return d}});const d=[{value:"hst-react-ui",paraId:0,tocIndex:0},{value:" \u4E3A\u516C\u53F8\u5E94\u7528\u63D0\u4F9B\u4E86\u4E30\u5BCC\u7684\u4E1A\u52A1\u7EC4\u4EF6\uFF0C\u5927\u5927\u63D0\u5347\u4E86\u56E2\u961F\u7684\u5F00\u53D1\u6548\u7387\u3002",paraId:0,tocIndex:0}]},9978:function(a,t,n){n.r(t),n.d(t,{texts:function(){return d}});const d=[{value:"hst-react-ui",paraId:0,tocIndex:0},{value:" \u662F\u57FA\u4E8E ",paraId:0,tocIndex:0},{value:"antd@5.x.x",paraId:0,tocIndex:0},{value:" \u5F00\u53D1\u7684\u3002",paraId:0,tocIndex:0},{value:"\u8BF7\u5148\u68C0\u67E5\u9879\u76EE\u4E2D\u662F\u5426\u5DF2\u5B89\u88C5\u4EE5\u4E0B\u4F9D\u8D56\uFF1A",paraId:1,tocIndex:1},{value:`"react": ">=16.9.0",
+"react-dom": ">=16.9.0",
+"antd": ">=5.0.0"
+`,paraId:2,tocIndex:1},{value:"hst-react-ui",paraId:3,tocIndex:2},{value:" \u53D1\u5E03\u5230\u516C\u53F8\u7684npm\u79C1\u670D",paraId:3,tocIndex:2},{value:`npm config set registry http://npm.xxx.com
+# or
+yarn config set registry http://npm.xxx.com
+`,paraId:4,tocIndex:2},{value:`npm install hst-react-ui
+# or
+yarn add hst-react-ui
+# or
+pnpm install hst-react-ui
+`,paraId:5,tocIndex:3}]},51446:function(a,t,n){n.r(t),n.d(t,{texts:function(){return d}});const d=[]}}]);
