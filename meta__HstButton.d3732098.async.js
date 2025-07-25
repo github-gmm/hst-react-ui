8f322c5bbb3e923d5e07f4ff2868981d786fe8a0
github-gmm <github-gmm@users.noreply.github.com>
2025-07-25 06:16:04 +0000

    deploy: 47292e39b16fe3b9bdae02fc4b1939dcd8899ce0

---
+++ meta__HstButton.d3732098.async.js
@@ -0,0 +1,233 @@
+"use strict";(self.webpackChunkhst_react_ui=self.webpackChunkhst_react_ui||[]).push([[838],{81073:function(B,l,t){var s;t.r(l),t.d(l,{demos:function(){return K}});var f=t(15009),d=t.n(f),M=t(99289),y=t.n(M),o=t(67294),x=t(61006),v=t(63743),T=t(6424),K={"hstbutton-demo-base":{component:o.memo(o.lazy(function(){return t.e(765).then(t.bind(t,99178))})),asset:{type:"BLOCK",id:"hstbutton-demo-base",refAtomIds:["HstButton"],dependencies:{"index.tsx":{type:"FILE",value:t(94771).Z},antd:{type:"NPM",value:"5.26.6"},"hst-react-ui":{type:"NPM",value:"0.0.2"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u5B9E\u5FC3"},context:{antd:x,"hst-react-ui":v,react:s||(s=t.t(o,2))},renderOpts:{compile:function(){var i=y()(d()().mark(function r(){var e,p=arguments;return d()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,t.e(250).then(t.bind(t,90250));case 2:return n.abrupt("return",(e=n.sent).default.apply(e,p));case 3:case"end":return n.stop()}},r)}));function c(){return i.apply(this,arguments)}return c}()}},"hstbutton-demo-base5":{component:o.memo(o.lazy(function(){return t.e(765).then(t.bind(t,20273))})),asset:{type:"BLOCK",id:"hstbutton-demo-base5",refAtomIds:["HstButton"],dependencies:{"index.tsx":{type:"FILE",value:t(81583).Z},antd:{type:"NPM",value:"5.26.6"},"hst-react-ui":{type:"NPM",value:"0.0.2"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u65E0\u8FB9\u6846"},context:{antd:x,"hst-react-ui":v,react:s||(s=t.t(o,2))},renderOpts:{compile:function(){var i=y()(d()().mark(function r(){var e,p=arguments;return d()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,t.e(250).then(t.bind(t,90250));case 2:return n.abrupt("return",(e=n.sent).default.apply(e,p));case 3:case"end":return n.stop()}},r)}));function c(){return i.apply(this,arguments)}return c}()}},"hstbutton-demo-base1":{component:o.memo(o.lazy(function(){return t.e(765).then(t.bind(t,93139))})),asset:{type:"BLOCK",id:"hstbutton-demo-base1",refAtomIds:["HstButton"],dependencies:{"index.tsx":{type:"FILE",value:t(94163).Z},antd:{type:"NPM",value:"5.26.6"},"hst-react-ui":{type:"NPM",value:"0.0.2"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u7A7A\u5FC3"},context:{antd:x,"hst-react-ui":v,react:s||(s=t.t(o,2))},renderOpts:{compile:function(){var i=y()(d()().mark(function r(){var e,p=arguments;return d()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,t.e(250).then(t.bind(t,90250));case 2:return n.abrupt("return",(e=n.sent).default.apply(e,p));case 3:case"end":return n.stop()}},r)}));function c(){return i.apply(this,arguments)}return c}()}},"hstbutton-demo-base4":{component:o.memo(o.lazy(function(){return t.e(765).then(t.bind(t,4457))})),asset:{type:"BLOCK",id:"hstbutton-demo-base4",refAtomIds:["HstButton"],dependencies:{"index.tsx":{type:"FILE",value:t(40281).Z},antd:{type:"NPM",value:"5.26.6"},"hst-react-ui":{type:"NPM",value:"0.0.2"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u865A\u7EBF"},context:{antd:x,"hst-react-ui":v,react:s||(s=t.t(o,2))},renderOpts:{compile:function(){var i=y()(d()().mark(function r(){var e,p=arguments;return d()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,t.e(250).then(t.bind(t,90250));case 2:return n.abrupt("return",(e=n.sent).default.apply(e,p));case 3:case"end":return n.stop()}},r)}));function c(){return i.apply(this,arguments)}return c}()}},"hstbutton-demo-base2":{component:o.memo(o.lazy(function(){return t.e(765).then(t.bind(t,46223))})),asset:{type:"BLOCK",id:"hstbutton-demo-base2",refAtomIds:["HstButton"],dependencies:{"index.tsx":{type:"FILE",value:t(43499).Z},antd:{type:"NPM",value:"5.26.6"},"hst-react-ui":{type:"NPM",value:"0.0.2"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u6587\u672C"},context:{antd:x,"hst-react-ui":v,react:s||(s=t.t(o,2))},renderOpts:{compile:function(){var i=y()(d()().mark(function r(){var e,p=arguments;return d()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,t.e(250).then(t.bind(t,90250));case 2:return n.abrupt("return",(e=n.sent).default.apply(e,p));case 3:case"end":return n.stop()}},r)}));function c(){return i.apply(this,arguments)}return c}()}},"hstbutton-demo-base3":{component:o.memo(o.lazy(function(){return t.e(765).then(t.bind(t,57695))})),asset:{type:"BLOCK",id:"hstbutton-demo-base3",refAtomIds:["HstButton"],dependencies:{"index.tsx":{type:"FILE",value:t(79862).Z},"@ant-design/icons":{type:"NPM",value:"6.0.0"},antd:{type:"NPM",value:"5.26.6"},"hst-react-ui":{type:"NPM",value:"0.0.2"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u52A0\u8F7D"},context:{"@ant-design/icons":T,antd:x,"hst-react-ui":v,react:s||(s=t.t(o,2))},renderOpts:{compile:function(){var i=y()(d()().mark(function r(){var e,p=arguments;return d()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,t.e(250).then(t.bind(t,90250));case 2:return n.abrupt("return",(e=n.sent).default.apply(e,p));case 3:case"end":return n.stop()}},r)}));function c(){return i.apply(this,arguments)}return c}()}},"hstbutton-demo-base7":{component:o.memo(o.lazy(function(){return t.e(765).then(t.bind(t,18320))})),asset:{type:"BLOCK",id:"hstbutton-demo-base7",refAtomIds:["HstButton"],dependencies:{"index.tsx":{type:"FILE",value:t(75448).Z},"@ant-design/icons":{type:"NPM",value:"6.0.0"},antd:{type:"NPM",value:"5.26.6"},"hst-react-ui":{type:"NPM",value:"0.0.2"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u7981\u7528"},context:{"@ant-design/icons":T,antd:x,"hst-react-ui":v,react:s||(s=t.t(o,2))},renderOpts:{compile:function(){var i=y()(d()().mark(function r(){var e,p=arguments;return d()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,t.e(250).then(t.bind(t,90250));case 2:return n.abrupt("return",(e=n.sent).default.apply(e,p));case 3:case"end":return n.stop()}},r)}));function c(){return i.apply(this,arguments)}return c}()}},"hstbutton-demo-base6":{component:o.memo(o.lazy(function(){return t.e(765).then(t.bind(t,46295))})),asset:{type:"BLOCK",id:"hstbutton-demo-base6",refAtomIds:["HstButton"],dependencies:{"index.tsx":{type:"FILE",value:t(74638).Z},antd:{type:"NPM",value:"5.26.6"},"hst-react-ui":{type:"NPM",value:"0.0.2"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u5757"},context:{antd:x,"hst-react-ui":v,react:s||(s=t.t(o,2))},renderOpts:{compile:function(){var i=y()(d()().mark(function r(){var e,p=arguments;return d()().wrap(function(n){for(;;)switch(n.prev=n.next){case 0:return n.next=2,t.e(250).then(t.bind(t,90250));case 2:return n.abrupt("return",(e=n.sent).default.apply(e,p));case 3:case"end":return n.stop()}},r)}));function c(){return i.apply(this,arguments)}return c}()}}}},63743:function(B,l,t){t.r(l),t.d(l,{HstButton:function(){return n},HstPopup:function(){return tt},HstTable:function(){return ot}});var s=t(97857),f=t.n(s),d=t(15009),M=t.n(d),y=t(99289),o=t.n(y),x=t(13769),v=t.n(x),T=t(5574),K=t.n(T),i=t(40238),c=t(83622),r=t(67294),e=t(85893),p=["onClick","variant","color","size","children"],P=function(u){var h=(0,r.useState)(!1),g=K()(h,2),I=g[0],S=g[1],m=u.onClick,O=u.variant,b=O===void 0?"solid":O,C=u.color,L=C===void 0?"primary":C,N=u.size,R=N===void 0?"middle":N,Z=u.children,z=v()(u,p),A=(0,i.Z)(function(){var D=o()(M()().mark(function E($){return M()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:return S(!0),a.prev=1,a.next=4,m==null?void 0:m($);case 4:return a.prev=4,S(!1),a.finish(4);case 7:case"end":return a.stop()}},E,null,[[1,,4,7]])}));return function(E){return D.apply(this,arguments)}}(),{wait:300}),W=A.run,F=["link"].includes(b)?"0":void 0,j=["small"].includes(R)?"12px":void 0;return(0,e.jsx)(c.ZP,f()(f()({variant:b,color:L,size:R,loading:I,onClick:W,style:{padding:F,fontSize:j}},z),{},{children:Z}))},n=(0,r.memo)(P),G=t(85265),J=["children","className","footer","onClosed"],Q=function(u){var h=u.children,g=h===void 0?null:h,I=u.className,S=u.footer,m=u.onClosed,O=v()(u,J),b=(0,r.useCallback)(function(){m==null||m()},[m]);return(0,e.jsx)(G.Z,f()(f()({className:"hst-popup-content".concat(I?"".concat(I):""),open:!0,maskClosable:!1,onClose:b,extra:S},O),{},{children:g}))},V=(0,r.memo)(Q),X=t(85576),Y=["width","children","footer","className","onClosed"],k=function(u){var h=u.width,g=h===void 0?378:h,I=u.children,S=I===void 0?null:I,m=u.footer,O=m===void 0?(0,e.jsx)(e.Fragment,{}):m,b=u.className,C=u.onClosed,L=v()(u,Y),N=(0,r.useCallback)(function(){C==null||C()},[C]);return(0,e.jsx)(X.Z,f()(f()({className:"hst-popup-content".concat(b?"".concat(b):""),open:!0,width:g,maskClosable:!1,onCancel:N,footer:O},L),{},{children:S}))},w=(0,r.memo)(k),q=["type","children","content","footer","beforeOpen","beforeClose"],_=(0,r.forwardRef)(function(H,u){var h=H.type,g=h===void 0?"modal":h,I=H.children,S=I===void 0?(0,e.jsx)(e.Fragment,{}):I,m=H.content,O=m===void 0?(0,e.jsx)(e.Fragment,{}):m,b=H.footer,C=H.beforeOpen,L=H.beforeClose,N=v()(H,q),R=(0,r.useState)(!1),Z=K()(R,2),z=Z[0],A=Z[1],W=g==="modal",F=function(){var D=o()(M()().mark(function E(){var $;return M()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:if(!C){a.next=6;break}return a.next=3,C();case 3:a.t0=a.sent,a.next=7;break;case 6:a.t0=!0;case 7:$=a.t0,A($);case 9:case"end":return a.stop()}},E)}));return function(){return D.apply(this,arguments)}}(),j=function(){var D=o()(M()().mark(function E(){var $;return M()().wrap(function(a){for(;;)switch(a.prev=a.next){case 0:if(!L){a.next=6;break}return a.next=3,L();case 3:a.t0=a.sent,a.next=7;break;case 6:a.t0=!0;case 7:$=a.t0,A(!$);case 9:case"end":return a.stop()}},E)}));return function(){return D.apply(this,arguments)}}();return(0,r.useImperativeHandle)(u,function(){return{onClose:j,onOpen:F}}),(0,e.jsxs)(e.Fragment,{children:[(0,e.jsx)("div",{onClick:F,children:S}),z&&(W?(0,e.jsx)(w,f()(f()({onClosed:j,footer:b},N),{},{children:O})):(0,e.jsx)(V,f()(f()({onClose:j,footer:b},N),{},{children:O})))]})}),tt=(0,r.memo)(_),nt=t(31112),et=["tableKey"],at=function(u){var h=u.tableKey,g=h===void 0?"id":h,I=v()(u,et);return(0,e.jsx)(nt.Z,f()({rowKey:g,options:{reload:!1,setting:!1,density:!1},pagination:{showQuickJumper:!0}},I))},ot=(0,r.memo)(at)},2444:function(B,l,t){t.r(l),t.d(l,{texts:function(){return s}});const s=[{value:"\u6309\u94AE\u7EC4\u4EF6 HstButton",paraId:0,tocIndex:1},{value:`import { HstButton } from 'hst-react-ui';
+
+export default () => <HstButton>\u6309\u94AE</HstButton>;
+`,paraId:1,tocIndex:2},{value:"\u5C5E\u6027",paraId:2,tocIndex:13},{value:"\u7C7B\u578B",paraId:2,tocIndex:13},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:13},{value:"\u8BF4\u660E",paraId:2,tocIndex:13},{value:"variant",paraId:2,tocIndex:13},{value:"variantType",paraId:2,tocIndex:13},{value:"solid",paraId:2,tocIndex:13},{value:"\u6309\u94AE\u7C7B\u578B",paraId:2,tocIndex:13},{value:"color",paraId:2,tocIndex:13},{value:"PresetColors",paraId:2,tocIndex:13},{value:"primary",paraId:2,tocIndex:13},{value:"\u6309\u94AE\u989C\u8272",paraId:2,tocIndex:13},{value:"block",paraId:2,tocIndex:13},{value:"boolean",paraId:2,tocIndex:13},{value:"false",paraId:2,tocIndex:13},{value:"\u6309\u94AE\u5BBD\u5EA6\u8C03\u6574\u4E3A\u7236\u5BBD\u5EA6\u7684\u9009\u9879",paraId:2,tocIndex:13},{value:"disable",paraId:2,tocIndex:13},{value:"boolean",paraId:2,tocIndex:13},{value:"false",paraId:2,tocIndex:13},{value:"\u7981\u7528\u72B6\u6001",paraId:2,tocIndex:13},{value:"onClick",paraId:2,tocIndex:13},{value:"(e) => Promise<void> / void",paraId:2,tocIndex:13},{value:"\u70B9\u51FB\u6309\u94AE\u56DE\u8C03",paraId:2,tocIndex:13},{value:"solid",paraId:3,tocIndex:14},{value:" ",paraId:3,tocIndex:14},{value:"outlined",paraId:3,tocIndex:14},{value:" ",paraId:3,tocIndex:14},{value:"dashed",paraId:3,tocIndex:14},{value:" ",paraId:3,tocIndex:14},{value:"filled",paraId:3,tocIndex:14},{value:" ",paraId:3,tocIndex:14},{value:"link",paraId:3,tocIndex:14},{value:"primary",paraId:4,tocIndex:15},{value:" ",paraId:4,tocIndex:15},{value:"default",paraId:4,tocIndex:15},{value:" ",paraId:4,tocIndex:15},{value:"blue",paraId:4,tocIndex:15},{value:" ",paraId:4,tocIndex:15},{value:"purple",paraId:4,tocIndex:15},{value:" ",paraId:4,tocIndex:15},{value:"cyan",paraId:4,tocIndex:15},{value:" ",paraId:4,tocIndex:15},{value:"green",paraId:4,tocIndex:15},{value:" ",paraId:4,tocIndex:15},{value:"magenta",paraId:4,tocIndex:15},{value:" ",paraId:4,tocIndex:15},{value:"pink",paraId:4,tocIndex:15},{value:" ",paraId:4,tocIndex:15},{value:"red",paraId:4,tocIndex:15},{value:" ",paraId:4,tocIndex:15},{value:"orange",paraId:4,tocIndex:15},{value:" ",paraId:4,tocIndex:15},{value:"yellow",paraId:4,tocIndex:15},{value:" ",paraId:4,tocIndex:15},{value:"volcano",paraId:4,tocIndex:15},{value:" ",paraId:4,tocIndex:15},{value:"geekblue",paraId:4,tocIndex:15},{value:" ",paraId:4,tocIndex:15},{value:"lime",paraId:4,tocIndex:15},{value:" ",paraId:4,tocIndex:15},{value:"gold",paraId:4,tocIndex:15}]},94771:function(B,l){l.Z=`import { Space } from 'antd';
+import { HstButton } from 'hst-react-ui';
+import React from 'react';
+
+export default () => {
+  return (
+    <Space wrap>
+      <HstButton>solid button</HstButton>
+      <HstButton color="green">solid button</HstButton>
+      <HstButton color="red">solid button</HstButton>
+      <HstButton color="orange">solid button</HstButton>
+      <HstButton color="default">solid button</HstButton>
+    </Space>
+  );
+};
+`},94163:function(B,l){l.Z=`import { Space } from 'antd';
+import { HstButton } from 'hst-react-ui';
+import React from 'react';
+
+export default () => {
+  return (
+    <Space wrap>
+      <HstButton variant="outlined">outlined button</HstButton>
+      <HstButton variant="outlined" color="green">
+        outlined button
+      </HstButton>
+      <HstButton variant="outlined" color="red">
+        outlined button
+      </HstButton>
+      <HstButton variant="outlined" color="orange">
+        outlined button
+      </HstButton>
+      <HstButton variant="outlined" color="default">
+        outlined button
+      </HstButton>
+    </Space>
+  );
+};
+`},43499:function(B,l){l.Z=`import { Space } from 'antd';
+import { HstButton } from 'hst-react-ui';
+import React from 'react';
+
+export default () => {
+  return (
+    <Space wrap>
+      <HstButton variant="link">primary color</HstButton>
+      <HstButton variant="link" color="default">
+        default color
+      </HstButton>
+      <HstButton variant="link" color="green">
+        green color
+      </HstButton>
+      <HstButton variant="link" color="red">
+        red color
+      </HstButton>
+      <HstButton variant="link" color="orange">
+        orange color
+      </HstButton>
+      <HstButton variant="link" color="blue">
+        blue color
+      </HstButton>
+      <HstButton variant="link" color="geekblue">
+        geekblue color
+      </HstButton>
+      <HstButton variant="link" color="cyan">
+        cyan color
+      </HstButton>
+      <HstButton variant="link" color="magenta">
+        magenta color
+      </HstButton>
+      <HstButton variant="link" color="pink">
+        pink color
+      </HstButton>
+      <HstButton variant="link" color="yellow">
+        yellow color
+      </HstButton>
+      <HstButton variant="link" color="volcano">
+        volcano color
+      </HstButton>
+      <HstButton variant="link" color="lime">
+        lime color
+      </HstButton>
+      <HstButton variant="link" color="gold">
+        gold color
+      </HstButton>
+    </Space>
+  );
+};
+`},79862:function(B,l){l.Z=`import { SyncOutlined } from '@ant-design/icons';
+import { Space } from 'antd';
+import { HstButton } from 'hst-react-ui';
+import React from 'react';
+
+export default () => {
+  const handleClick = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('\u6210\u529F\u4E86');
+      }, 3000);
+    });
+  };
+
+  const handleClick1 = async () => {
+    const res = await handleClick();
+    console.log(res);
+  };
+
+  return (
+    <Space wrap>
+      <HstButton icon={<SyncOutlined />} onClick={handleClick}>
+        loading button
+      </HstButton>
+
+      <HstButton
+        icon={<SyncOutlined />}
+        variant="outlined"
+        color="default"
+        onClick={handleClick}
+      >
+        loading button
+      </HstButton>
+
+      <HstButton
+        variant="dashed"
+        icon={<SyncOutlined />}
+        onClick={handleClick1}
+      >
+        loading button
+      </HstButton>
+
+      <HstButton
+        variant="filled"
+        icon={<SyncOutlined />}
+        onClick={handleClick1}
+      >
+        loading button
+      </HstButton>
+
+      <HstButton variant="link" icon={<SyncOutlined />} onClick={handleClick1}>
+        loading button
+      </HstButton>
+    </Space>
+  );
+};
+`},40281:function(B,l){l.Z=`import { Space } from 'antd';
+import { HstButton } from 'hst-react-ui';
+import React from 'react';
+
+export default () => {
+  return (
+    <Space wrap>
+      <HstButton variant="dashed">dashed button</HstButton>
+      <HstButton variant="dashed" color="green">
+        dashed button
+      </HstButton>
+      <HstButton variant="dashed" color="red">
+        dashed button
+      </HstButton>
+      <HstButton variant="dashed" color="orange">
+        dashed button
+      </HstButton>
+      <HstButton variant="dashed" color="default">
+        dashed button
+      </HstButton>
+    </Space>
+  );
+};
+`},81583:function(B,l){l.Z=`import { Space } from 'antd';
+import { HstButton } from 'hst-react-ui';
+import React from 'react';
+
+export default () => {
+  return (
+    <Space wrap>
+      <HstButton variant="filled">filled button</HstButton>
+      <HstButton variant="filled" color="green">
+        filled button
+      </HstButton>
+      <HstButton variant="filled" color="red">
+        filled button
+      </HstButton>
+      <HstButton variant="filled" color="orange">
+        filled button
+      </HstButton>
+      <HstButton variant="filled" color="default">
+        filled button
+      </HstButton>
+    </Space>
+  );
+};
+`},74638:function(B,l){l.Z=`import { Flex } from 'antd';
+import { HstButton } from 'hst-react-ui';
+import React from 'react';
+
+export default () => {
+  return (
+    <Flex vertical gap="small" style={{ width: '100%' }}>
+      <HstButton block>block button</HstButton>
+      <HstButton variant="outlined" block>
+        block button
+      </HstButton>
+      <HstButton variant="filled" block>
+        block button
+      </HstButton>
+    </Flex>
+  );
+};
+`},75448:function(B,l){l.Z=`import { SyncOutlined } from '@ant-design/icons';
+import { Space } from 'antd';
+import { HstButton } from 'hst-react-ui';
+import React from 'react';
+
+export default () => {
+  return (
+    <Space wrap>
+      <HstButton icon={<SyncOutlined />} color="default" disabled>
+        disabled button
+      </HstButton>
+
+      <HstButton icon={<SyncOutlined />} variant="dashed" disabled>
+        disabled button
+      </HstButton>
+
+      <HstButton icon={<SyncOutlined />} variant="link" disabled>
+        disabled button
+      </HstButton>
+    </Space>
+  );
+};
+`}}]);
